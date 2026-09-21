a035d260c36f719e6fd2707a87dd34cdf91f2b81
Ujwalushettigar <ujwalshettigar@gmail.com>
2026-09-21 09:56:34 +0530

    Time: 0 ms (100%), Space: 10.5 MB (73.44%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool fun(int c,vector<int>& w,int days)
+    {
+        int day=1;
+        int sum=0;
+        for(int i:w)
+        {
+            if(sum+i<=c)
+            {
+                sum+=i;
+            }
+            else if(sum+i>c)
+            {
+                day+=1;
+                sum=i;
+            }
+        }
+        return day<=days;
+    }
+    int splitArray(vector<int>& weights, int days) {
+        int l=*max_element(weights.begin(),weights.end());
+        int r=accumulate(weights.begin(),weights.end(),0);
+        int m=0;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(fun(mid,weights,days))
+            {
+                r=mid-1;
+                m=mid;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return m;
+    }
+};
